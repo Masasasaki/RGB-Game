1a7095335d32028f17a286f0938fb142d646b40b
Masaki <masakiasanuma@outlook.com>
2020-04-04 11:45:48 -0700

    Updated js syntax to ES6.

--- colorgame.js
+++ colorgame.js
@@ -1,93 +1,87 @@
-var numCircles = 9;
-var colors = [];
-var pickedColor;
-var circles = document.querySelectorAll(".circle");
-var colorDisplay = document.getElementById("colorDisplay");
-var messageDisplay = document.querySelector("#message");
-var h1 = document.querySelector("h1");
-var resetButton = document.querySelector("#reset");
-var modeButtons = document.querySelectorAll(".mode");
+let numCircles = 9
+let colors = []
+let pickedColor
+let circles = document.querySelectorAll(".circle")
+let colorDisplay = document.getElementById("colorDisplay")
+let messageDisplay = document.querySelector("#message")
+let h1 = document.querySelector("h1")
+let resetButton = document.querySelector("#reset")
+let modeButtons = document.querySelectorAll(".mode")
 
-init();
-
-function init() {
-    setupModeButtons();
-    setupCircles();
-    reset();
+const changeColors = color => {
+    circles.forEach(circle => circle.style.background = color)
 }
 
-function setupModeButtons() {
-    for (var i = 0; i < modeButtons.length; i++) {
-        modeButtons[i].addEventListener("click", function(){
-            modeButtons[0].classList.remove("selected");
-            modeButtons[1].classList.remove("selected");
-            this.classList.add("selected");
-            this.textContent === "Easy" ? numCircles = 3 : numCircles = 9;
-            reset();
-        });
+const pickColor = () => {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
+
+const generateRandomColors = num => {
+    let arr = []
+    for (let i = 0; i < num; i++) {
+        arr.push(randomColor())
     }
+    return arr
 }
 
-function setupCircles() {
-    for (var i = 0; i < circles.length; i++) {
-        circles[i].addEventListener("click", function(){
-            var clickedColor = this.style.backgroundColor;
+const randomColor = () => {
+    let r = Math.floor(Math.random() * 256)
+    let g = Math.floor(Math.random() * 256)
+    let b = Math.floor(Math.random() * 256)
+    return "rgb(" + r + ", " + g + ", " + b + ")"
+}
+
+const setupModeButtons = () => {
+    modeButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            modeButtons[0].classList.remove("selected")
+            modeButtons[1].classList.remove("selected")
+            button.classList.add("selected")
+            button.textContent === "Easy" ? numCircles = 3 : numCircles = 9
+            reset()
+        })
+    })
+}
+
+const setupCircles = () => {
+    circles.forEach(circle => {
+        circle.addEventListener("click", function(){
+            let clickedColor = this.style.backgroundColor
             if (clickedColor === pickedColor) {
-                messageDisplay.textContent = "Correct!";
-                resetButton.textContent = "Play Again?";
-                changeColors(clickedColor);
-                h1.style.backgroundColor = clickedColor;
+                messageDisplay.textContent = "Correct!"
+                resetButton.textContent = "Play Again?"
+                changeColors(clickedColor)
+                h1.style.backgroundColor = clickedColor
             } else {
-                this.style.backgroundColor = "#232323";
-                messageDisplay.textContent = "Try Again";
+                this.style.backgroundColor = "#232323"
+                messageDisplay.textContent = "Try Again"
             }
-        });
-    }
+        })
+    })
 }
 
-function reset() {
-    colors = generateRandomColors(numCircles);
-    pickedColor = pickColor();
-    colorDisplay.textContent = pickedColor;
-    messageDisplay.textContent = "";
-    resetButton.textContent = "New Colors";
-    for (var i = 0; i < circles.length; i++) {
+const reset = () => {
+    colors = generateRandomColors(numCircles)
+    pickedColor = pickColor()
+    colorDisplay.textContent = pickedColor
+    messageDisplay.textContent = ""
+    resetButton.textContent = "New Colors"
+    circles.forEach((circle, i) => {
         if (colors[i]) { 
-            circles[i].style.display = "block";
-            circles[i].style.background = colors[i];
+            circle.style.display = "block"
+            circle.style.background = colors[i]
         } else {
-            circles[i].style.display = "none";
+            circle.style.display = "none"
         } 
-    }
-    h1.style.backgroundColor = "black";
+    })
+    h1.style.backgroundColor = "black"
 }
 
-resetButton.addEventListener("click", function(){
-    reset();
-});
-
-function changeColors(color) {
-    for (var i = 0; i < circles.length; i++) {
-        circles[i].style.background = color;
-    }
+const init = () => {
+    setupModeButtons()
+    setupCircles()
+    reset()
 }
 
-function pickColor() {
-    var random = Math.floor(Math.random() * colors.length);
-    return colors[random];
-}
-
-function generateRandomColors(num) {
-    var arr = [];
-    for (var i = 0; i < num; i++) {
-        arr.push(randomColor());
-    }
-    return arr;
-}
-
-function randomColor() {
-    var r = Math.floor(Math.random() * 256);
-    var g = Math.floor(Math.random() * 256);
-    var b = Math.floor(Math.random() * 256);
-    return "rgb(" + r + ", " + g + ", " + b + ")";
-}
+init()
+resetButton.addEventListener("click", () => reset())
